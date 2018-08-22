dcc5e806b1327269a8c3aa8bc6b936de3007da60
Sean Phelan <seanphelan97@gmail.com>
2018-08-22 12:05:32 -0600

    updated person object with new function

--- timothy/4.js
+++ timothy/4.js
@@ -1,12 +1,23 @@
-function Person(name, jobTitle){
+function Person(name, jobTitle, boss){
     this.name = name;
     this.jobTitle = jobTitle;
+    this.boss = boss;
 }
+
+Person.prototype.getSuperior = function() {
+      // if no boss is set assume that someone doesn't have a boss
+      if (this.boss) {
+        return `${this.jobTitle} ${this.name} reports to ${this.boss}.`;
+      } else {
+        return `${this.jobTitle} ${this.name} doesn't report to anybody.`;
+      }
+    }
 
 const personsArray = [
     new Person("Sherlock Holmes", "Detective"),
     new Person("Abraham Lincoln", "U.S. President"),
-    new Person("Martin Luther King Jr", "Civil Rights Leader")
+    new Person("Martin Luther King Jr", "Civil Rights Leader"),
+    new Person("Bob", "Software engineer", "somebody")
 ];
 
-personsArray.forEach(person => console.log(`${person.name} is a ${person.jobTitle}.`));
+personsArray.forEach(person => console.log(person.getSuperior()));
