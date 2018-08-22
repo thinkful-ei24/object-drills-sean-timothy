761fc4e26e74e84e561b18845833c0e8c425f527
Timothy Chang <superfluous11@gmail.com>
2018-08-22 08:54:28 -0700

    completed deleting-keys.js

---
+++ deleting-keys.js
@@ -0,0 +1,16 @@
+const sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  };
+  
+  function keyDeleter(obj) {
+    delete obj['foo'];
+    delete obj['bar'];
+    return obj;
+  }
+
+  const testObject = keyDeleter(sampleObj);
+  console.log(testObject);
+  
